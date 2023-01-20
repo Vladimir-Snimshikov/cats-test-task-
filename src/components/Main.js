8178df3fb04e3ae9205ef5f9b1d39f2be4110835
Vladimir Snimshikov <vladimirsnimshikov@yandex.ru>
2023-01-20 17:09:16 +0400

    refactor:сделать добавление компонентов Кет через меп

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,54 +1,24 @@
 import Cat from './Cat';
 import React from 'react';
+import { initionalCats } from '../utils/initionalCats';
 export default function Main() {
   return (
     <main className="content">
       <section className="cats">
         <h2 className="cats__title"> Ты сегодня покормил кота?</h2>
         <ul className="cards">
-          <Cat
-            catsTitle="Нямушка"
-            catsSubtitle="с курой"
-            catsOvalText="0,5"
-            catsDescription="Чего сидишь? Порадуй котэ"
-            id={1}
-          >
-            <p className="cards__description">
-              <span>10</span>порций
-            </p>
-            <p className="cards__description">мышь в подарок</p>
-          </Cat>
-          <Cat
-            catsTitle="Нямушка"
-            catsSubtitle="с курой"
-            catsOvalText="2"
-            catsDescription="Чего сидишь? Порадуй котэ"
-            id={2}
-          >
-            <p className="cards__description">
-              <span>40</span>порций
-            </p>
-
-            <p className="cards__description">
-              <span>2</span>мыши в подарок
-            </p>
-          </Cat>
-          <Cat
-            catsTitle="Нямушка"
-            catsSubtitle="с курой"
-            catsOvalText="5"
-            catsDescription="Чего сидишь? Порадуй котэ"
-            id={3}
-          >
-            <p className="cards__description">
-              <span>100</span>порций
-            </p>
-
-            <p className="cards__description">
-              <span>5</span>мышей в подарок
-            </p>
-            <p className="cards__description">заказчик доволен</p>
-          </Cat>
+          {initionalCats.map((cat) => {
+            return (
+              <Cat
+                key={cat.catsId}
+                catsTitle={cat.catsTitle}
+                catsSubtitle={cat.catsSubtitle}
+                catsOvalText={cat.catsOvalText}
+                catsDescription={cat.catsDescription}
+                id={cat.catsId}
+              ></Cat>
+            );
+          })}
         </ul>
       </section>
     </main>
---
+++ src/utils/initionalCats.js
@@ -0,0 +1,23 @@
+export const initionalCats = [
+  {
+    catsTitle: 'Нямушка',
+    catsSubtitle: 'с фуа-гра',
+    catsOvalText: '0,5',
+    catsDescription: 'Чего сидишь? Порадуй котэ',
+    catsId: 1,
+  },
+  {
+    catsTitle: 'Нямушка',
+    catsSubtitle: 'с рыбой',
+    catsOvalText: '2',
+    catsDescription: 'Чего сидишь? Порадуй котэ',
+    catsId: 2,
+  },
+  {
+    catsTitle: 'Нямушка',
+    catsSubtitle: 'с курой',
+    catsOvalText: '5',
+    catsDescription: 'Чего сидишь? Порадуй котэ',
+    catsId: 3,
+  },
+];
